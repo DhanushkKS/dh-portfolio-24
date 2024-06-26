eadaee9f69233187adcbae5334f7a3d244074213
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2024-06-26 09:05:35 +0530

    Feat: Created Contact buttons

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,6 +2,11 @@
 import Image from "next/image";
 import { profileImage } from "@/libs/data";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowBarRight, BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithub } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
 
 export const Intro = () => {
   return (
@@ -52,7 +57,50 @@ export const Intro = () => {
           building <span className="italic">sites & apps</span>. My focus is
           <span className="underline">.NET and React </span>.
         </motion.p>
-        <div>//</div>
+        <motion.div
+          className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2 }}
+        >
+          <Link
+            href="#contact"
+            className="group bg-gray-900 text-white px-7 py-3 flex
+            items-center rounded-full gap-2 outline-none focus:scale-110
+            hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          >
+            Contact me here
+            <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          </Link>
+
+          <a
+            href="/cv_shyamanDhanushka_0506.pdf"
+            download
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110
+            hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10"
+          >
+            Download CV
+            <HiDownload className="opacity-70 group-hover:translate-y-1" />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/shyaman-dhanushka"
+            target="_blank"
+            className="bg-white p-4 flex items-center gap-2 text-gray-700 rounded-full  focus:scale-[1.15]
+            hover:scale-[1.15]  active:scale-105 transition cursor-pointer border border-black/10"
+          >
+            <BsLinkedin />
+          </a>
+
+          <a
+            href="https://github.com/DhanushkKS"
+            target="_blank"
+            className="bg-white p-4 flex items-center gap-2 text-gray-700 rounded-full  focus:scale-[1.15]
+            hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          >
+            <FaGithubSquare />
+          </a>
+        </motion.div>
       </section>
     </>
   );
