f6387fc5b9487ba53538cc9d11c45b86023d97e8
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2024-07-07 19:58:25 +0530

    Feat: Created skill level bar

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -17,7 +17,7 @@ export const Skills = () => {
         <ul className="flex flex-wrap justify-center gap-2 text-lg">
           {skillsData.map((skill, index) => (
             <motion.li
-              className="bg-white border border-black/[0.1] rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+              className="relative bg-white border overflow-hidden border-black/[0.1] rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
               key={index}
               initial={{ opacity: 0, y: 100 }}
               // animate={{ opacity: 1, y: 0 }}
@@ -25,7 +25,17 @@ export const Skills = () => {
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
             >
-              {skill}
+              <span>{skill.name}</span>
+              <span
+                className="opacity-15  border border-gray-900 absolute bg-gradient-to-r from-gray-600 to-gray-900 shadow-xl backdrop-blur-sm h-full
+                bottom-0 left-0 rounded-lg overflow-hidden dark:bg-gradient-to-r
+                dark:from-gray-200 dark:to-gray-500 dark:shadow-lg dark:bg-opacity-10 dark:border dark:border-gray-900 "
+                style={{
+                  width: `${skill.level}%`,
+                  boxShadow:
+                    "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
+                }}
+              ></span>
             </motion.li>
           ))}
         </ul>
